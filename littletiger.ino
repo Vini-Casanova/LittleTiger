244ccc71b3080bf06a48c728651bfd7530203515
vinicius-casanova <vinicius.santos@netbiis.com>
2024-11-21 13:03:27 -0300

    Bluetooth

--- littletiger.ino
+++ littletiger.ino
@@ -1,10 +1,9 @@
-#include <PS2X_lib.h>
 
-#define dirFrente 5 
-#define dirTras   3
+#define dirFrente 25 
+#define dirTras   26
 
-#define esqFrente 9
-#define esqTras   6
+#define esqFrente 32
+#define esqTras   33
 
 #define FRENTE 1
 #define PARADO 0
@@ -13,24 +12,34 @@
 #define velocidadeE 100
 #define velocidadeD 110
 
-PS2X ps2x;
-
-void configuraControle() {
-  int erroControle = ps2x.config_gamepad(13, 11, 10, 12, true, true);
-  if (!erroControle) {
-    Serial.println("Controle encontrado");
-  } else {
-    Serial.println("Controle não encontrado");
-    while (erroControle) {
-      if (bitRead(millis(), 11)) erroControle = ps2x.config_gamepad(13, 11, 10, 12, true, true);
-    }
-  }
-}
+#include "BluetoothSerial.h"
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run 'make menuconfig' to and enable it
+#endif
+BluetoothSerial SerialBT;
+char ch;
+//
+//PS2X ps2x;
+//
+//void configuraControle() {
+//  int erroControle = ps2x.config_gamepad(13, 11, 10, 12, true, true);
+//  if (!erroControle) {
+//    Serial.println("Controle encontrado");
+//  } else {
+//    Serial.println("Controle não encontrado");
+//    while (erroControle) {
+//      if (bitRead(millis(), 11)) erroControle = ps2x.config_gamepad(13, 11, 10, 12, true, true);
+//    }
+//  }
+//}
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  configuraControle();
+  //configuraControle();
+
+  SerialBT.begin("LITTLE TIGER"); //Bluetooth device name
+  Serial.println("O dispositivo foi iniciado, agora você pode parar!");
 
   pinMode(dirFrente, OUTPUT);
   pinMode(dirTras,   OUTPUT);
@@ -46,42 +55,69 @@ void setup() {
 
 void andarFrente(){
   // MOTOR DIREITO PARA FRENTE
-    analogWrite(dirFrente,  velocidadeD);
-    analogWrite(dirTras,    LOW);
+//    analogWrite(dirFrente,  velocidadeD);
+//    analogWrite(dirTras,    LOW);
+
+    digitalWrite(dirFrente, HIGH);
+    digitalWrite(dirTras, LOW);
 
     // MOTOR ESQUERDO PARA FRENTE
-    analogWrite(esqFrente,  velocidadeE);
-    analogWrite(esqTras,    LOW);
+//    analogWrite(esqFrente,  velocidadeE);
+//    analogWrite(esqTras,    LOW);
+
+    digitalWrite(esqFrente, HIGH);
+    digitalWrite(esqTras, LOW);
 }
 
 void andarTras(){
   // MOTOR DIREITO PARA TRAS
-    analogWrite(dirFrente,  LOW);
-    analogWrite(dirTras,    velocidadeE);
+//    analogWrite(dirFrente,  LOW);
+//    analogWrite(dirTras,    velocidadeE);
+
+    parar();
+    delay(50);
+
+    digitalWrite(dirFrente, LOW);
+    digitalWrite(dirTras, HIGH);
 
     // MOTOR ESQUERDO PARA TRAS
-    analogWrite(esqFrente,  LOW);
-    analogWrite(esqTras,    velocidadeD);
+//    analogWrite(esqFrente,  LOW);
+//    analogWrite(esqTras,    velocidadeD);
+
+    digitalWrite(esqFrente, LOW);
+    digitalWrite(esqTras, HIGH);
 }
 
 void virarDireita(){
   // MOTOR DIREITO PARA TRAS
-    analogWrite(dirFrente,  LOW);
-    analogWrite(dirTras,    velocidadeD);
+//    analogWrite(dirFrente,  LOW);
+//    analogWrite(dirTras,    velocidadeD);
+
+    digitalWrite(dirFrente, HIGH);
+    digitalWrite(dirTras, LOW);
 
     // MOTOR ESQUERDO PARA FRENTE
-    analogWrite(esqFrente,  velocidadeE);
-    analogWrite(esqTras,    LOW);
+//    analogWrite(esqFrente,  velocidadeE);
+//    analogWrite(esqTras,    LOW);
+
+    digitalWrite(esqFrente, LOW);
+    digitalWrite(esqTras, LOW);
 }
 
 void virarEsquerda(){
   // MOTOR DIREITO PARA FRENTE
-    analogWrite(dirFrente,  velocidadeD);
-    analogWrite(dirTras,    LOW);
+//    analogWrite(dirFrente,  velocidadeD);
+//    analogWrite(dirTras,    LOW);
+
+    digitalWrite(dirFrente, LOW);
+    digitalWrite(dirTras, LOW);
 
     // MOTOR ESQUERDO PARA TRAS
-    analogWrite(esqFrente,  LOW);
-    analogWrite(esqTras,    velocidadeE);
+//    analogWrite(esqFrente,  LOW);
+//    analogWrite(esqTras,    velocidadeE);
+
+    digitalWrite(esqFrente, HIGH);
+    digitalWrite(esqTras, LOW);
 }
 
 void parar(){
@@ -93,22 +129,29 @@ void parar(){
 
 
 void loop() {
-  ps2x.read_gamepad();
-
-  if (ps2x.Button(PSB_PAD_UP)) {
-    andarFrente();
-  } else if (ps2x.Button(PSB_PAD_RIGHT)) {
-    virarDireita();
-  } else if (ps2x.Button(PSB_PAD_LEFT)) {
-    virarEsquerda();
-  } else if (ps2x.Button(PSB_PAD_DOWN)) {
-    andarTras();
-  } else {
-    parar();
-  }
+    if (Serial.available()) {
+       SerialBT.write(Serial.read());    }
+    if (SerialBT.available()) {
+    ch = SerialBT.read();
+    if (ch == 'A')  {andarFrente();}
+    if (ch == 'V')  {andarTras();}
+    if (ch == 'P')  {parar();}
+    if (ch == 'D')  {virarDireita();}
+    if (ch == 'E')  {virarEsquerda();}
+    }
+//  ps2x.read_gamepad();
+//
+//  if (ps2x.Button(PSB_PAD_UP)) {
+//    andarFrente();
+//  } else if (ps2x.Button(PSB_PAD_RIGHT)) {
+//    virarDireita();
+//  } else if (ps2x.Button(PSB_PAD_LEFT)) {
+//    virarEsquerda();
+//  } else if (ps2x.Button(PSB_PAD_DOWN)) {
+//    andarTras();
+//  } else {
+//    parar();
+//  }
 
   delay(50);
 }
-
-
-
